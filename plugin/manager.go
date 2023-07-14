fa9da79521797d2704328178e43b3aa8b6f02212
Pritesh Bandi <priteshbandi@gmail.com>
2023-07-13 23:04:07 -0700

    Support uber plugin
    Signed-off-by: Pritesh Bandi <priteshbandi@gmail.com>

--- plugin/manager.go
+++ plugin/manager.go
@@ -3,10 +3,12 @@ package plugin
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io/fs"
 	"path"
 
 	"github.com/notaryproject/notation-go/dir"
+	"github.com/notaryproject/notation-go/plugin/proto"
 )
 
 // ErrNotCompliant is returned by plugin methods when the response is not
@@ -20,6 +22,63 @@ var ErrNotRegularFile = errors.New("not regular file")
 type Manager interface {
 	Get(ctx context.Context, name string) (Plugin, error)
 	List(ctx context.Context) ([]string, error)
+}
+
+// UberManager implements Manager and supports both plugin as library as well as executable
+type UberManager struct {
+	cliManager *CLIManager
+	libPlugins map[string]Plugin
+}
+
+// NewUberManager returns Manager which supports both plugin as library as well as executable
+func NewUberManager(cxt context.Context, cliMgr *CLIManager, libs ...Plugin) (*UberManager, error) {
+	libPlugins := make(map[string]Plugin, len(libs))
+	for _, lib := range libs {
+		metadataResp, err := lib.GetMetadata(cxt, &proto.GetMetadataRequest{})
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize library plugin")
+		}
+		libPlugins[metadataResp.Name] = lib
+	}
+
+	return &UberManager{
+		cliManager: cliMgr,
+		libPlugins: libPlugins,
+	}, nil
+}
+
+// Get returns a plugin by its name. If the plugin is not found, the error is of type os.ErrNotExist.
+func (m *UberManager) Get(ctx context.Context, name string) (Plugin, error) {
+	if p, ok := m.libPlugins[name]; ok {
+		return p, nil
+	}
+
+	if m.cliManager != nil {
+		return m.cliManager.Get(ctx, name)
+	}
+
+	return nil, fmt.Errorf("'%s' plugin not found", name)
+}
+
+// List produces a list of the plugin names
+func (m *UberManager) List(ctx context.Context) ([]string, error) {
+	list := make([]string, len(m.libPlugins))
+
+	for name := range m.libPlugins {
+		list = append(list, name)
+	}
+
+	if m.cliManager != nil {
+		cliList, err := m.cliManager.List(ctx)
+		if err != nil {
+			return nil, err
+		}
+		for _, cliName := range cliList {
+			list = append(list, cliName)
+		}
+	}
+
+	return list, nil
 }
 
 // CLIManager implements Manager
@@ -47,7 +106,7 @@ func (m *CLIManager) Get(ctx context.Context, name string) (Plugin, error) {
 }
 
 // List produces a list of the plugin names on the system.
-func (m *CLIManager) List(ctx context.Context) ([]string, error) {
+func (m *CLIManager) List(_ context.Context) ([]string, error) {
 	var plugins []string
 	fs.WalkDir(m.pluginFS, ".", func(dir string, d fs.DirEntry, err error) error {
 		if err != nil {
